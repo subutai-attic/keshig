0d08cd821cdad608e079f9ae3d748c657a5d4b63
Abdyldaev Almaz <aabdyldaev@critical-factor.com>
2015-11-19 18:59:20 +0600

    Initial Commit6. SS-3629

--- qnd/deploy.sh
+++ qnd/deploy.sh
@@ -6,9 +6,15 @@ mgmt_folder=$1
 SNAP_DISTR="SNAPS/$mgmt_folder"
 rdpPort=3490
 
+pushd $my_dir
 vagrant destroy -f
-cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}' | tr -d '"' | awk  '{print "vboxmanage controlvm "$1" poweroff && vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
-cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}' | tr -d '"' | awk  '{print "vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
+
+vboxmanage list vms | awk -F" " '{print $1}' | tr -d '"' | awk  '{print "vboxmanage controlvm "$1" poweroff && vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
+vboxmanage list vms | awk -F" " '{print $1}' | tr -d '"' | awk  '{print "vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
+rm -rf "$HOME/VirtualBox VMs/"
+
+#cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}' | tr -d '"' | awk  '{print "vboxmanage controlvm "$1" poweroff && vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
+#cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}' | tr -d '"' | awk  '{print "vboxmanage unregistervm "$1" --delete "}' | bash > /dev/null 2>&1
 vagrant up
 vagrant ssh-config > ssh.config
 
@@ -41,6 +47,7 @@ fi
 function conf_run(){
 line=$1
 rdpPort=$2
+BRIDGE_INT=$3
 if [[ "$(echo $line | grep MGMT)" ]]; then
 VM="$line"
 VBoxManage modifyvm $VM --nic2 intnet --intnet2 intnetSnappy${line: -1}  --nicpromisc2 allow-all --macaddress2 auto
@@ -69,15 +76,15 @@ done < <(cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}'  | tr -d
 wait
 
 vagrant halt
-
+popd
 while IFS= read -r line; do
 echo "Configure and Run $line  rdp:$rdpPort"
-conf_run $line $rdpPort
+conf_run $line $rdpPort $BRIDGE_INT
 rdpPort=$((rdpPort+1))
 done < <(cat $my_dir/Vagrantfile | grep v.name | awk -F"=" '{print $2}'  | tr -d '"')
 echo "Waiting for Management IP"
 
-sleep 45
+sleep 55
 
 function get_ip {
 mac1=$(VBoxManage showvminfo SubutaiMGMT1 --machinereadable | grep macaddress1 | awk -F "=" '{print $2}' | tr -d '"')
