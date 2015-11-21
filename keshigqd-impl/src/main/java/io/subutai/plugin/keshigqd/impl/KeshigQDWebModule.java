52b2cf2287e10385cb7e6f29b938b732111f95af
istus <istus@mail.critical-factor.com>
2015-11-21 23:41:20 +0600

    SS-3858 keshig added, webmodule

---
+++ keshigqd-impl/src/main/java/io/subutai/plugin/keshigqd/impl/KeshigQDWebModule.java
@@ -0,0 +1,36 @@
+package io.subutai.plugin.keshigqd.impl;
+
+
+import io.subutai.webui.api.WebuiModule;
+
+
+public class KeshigQDWebModule implements WebuiModule
+{
+    public static String NAME = "Keshig";
+    public static String IMG = "plugins/cassandra/cassandra.png";
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+
+    @Override
+    public String getModuleInfo()
+    {
+        return String.format( "{\"img\" : \"%s\", \"name\" : \"%s\"}", IMG, NAME );
+    }
+
+
+    @Override
+    public String getAngularDependecyList()
+    {
+        return String.format( "{" +
+                "name: 'subutai.blueprints', files: ["
+                + "'subutai-app/blueprints/blueprints.js',"
+                + "'subutai-app/blueprints/controller.js',"
+                + "'subutai-app/environment/service.js'"
+                + "]}" );
+    }
+}
