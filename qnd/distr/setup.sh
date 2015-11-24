1a6d9b7bd9bf4201026bf360981327600d405797
Abdyldaev Almaz <aabdyldaev@critical-factor.com>
2015-11-24 18:52:22 +0600

    commitone. AS-357

--- qnd/distr/setup.sh
+++ qnd/distr/setup.sh
@@ -13,11 +13,11 @@ sudo snappy install --allow-unauthenticated $distr/*.snap
 systemctl | grep subutai-mng_subutai-mng | awk -F" " '{print $1}' | awk -F"." '{print " sudo systemctl stop "$1}'  | bash
 sudo find /var/lib/apps/subutai-mng  -name db -type d | awk '{print "sudo rm -rf "$1}' | bash
 sudo bash -c "echo 'Defaults secure_path="/apps/bin/:/apps/subutai/current/bin:/apps/subutai-mng/current/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' > /etc/sudoers.d/subutai"
-
+rm -rf $distr
 else
 
 sudo snappy install --allow-unauthenticated $distr/*.snap
 sudo bash -c "echo 'Defaults secure_path="/apps/subutai/current/bin:/apps/subutai-mng/current/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' > /etc/sudoers.d/subutai"
 sudo btrfsinit /dev/sdc
-
+rm -rf $distr
 fi
