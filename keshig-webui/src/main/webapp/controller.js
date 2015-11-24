b62e682c11c7c902471fc40f3c0c8721152a8e09
istus <istus@mail.critical-factor.com>
2015-11-24 17:32:07 +0600

    SS-3915 keshig, updated webui

--- keshig-webui/src/main/webapp/controller.js
+++ keshig-webui/src/main/webapp/controller.js
@@ -1,75 +1,490 @@
 'use strict';
 
 angular.module('subutai.plugins.keshig.controller', [])
-    .controller('KeshigCtrl', KeshigCtrl);
+    .controller('KeshigCtrl', KeshigCtrl)
+	.directive('checkboxListDropdown', checkboxListDropdown);
 
-KeshigCtrl.$inject = ['keshigSrv', 'SweetAlert'];
-function KeshigCtrl(keshigSrv, SweetAlert) {
+KeshigCtrl.$inject = ['$scope', 'keshigSrv', 'DTOptionsBuilder', 'DTColumnBuilder', '$resource', '$compile', 'SweetAlert', 'peerRegistrationService'];
+function KeshigCtrl($scope, keshigSrv, DTOptionsBuilder, DTColumnBuilder, $resource, $compile, SweetAlert, peerRegistrationService) {
     var vm = this;
 
-	vm.optionType = "clone";
+	vm.activeTab = 'servers';
+	vm.optionType = "CLONE";
 
-	vm.serverName = "";
-	vm.serverType = "";
+	vm.servers = {};
+	vm.server2Add = {};
+	vm.serverFormUpdate = false;
 
+	vm.serverTypes = [];
+	vm.resourceHosts = [];
+	vm.servers = {};
+
+	vm.optionsType = [];
+	vm.optionsDeployBuilds = [];
+	vm.option2Add = {};
+	vm.options = {};
+	vm.optionFormUpdate = false;
+
+	vm.profiles2Add = {};
+	vm.profilesFormUpdate = false;
+
+	vm.serversByType = {};	
+	vm.optionsByType = [];
+	vm.playbooks = [];
+
+	//functions
 	vm.updateOption = updateOption;
-	vm.updateServer = updateOption;
+	vm.updateServer = updateServer;
+	vm.updateProfile = updateProfile;
+
 	vm.deleteServer = deleteServer;
 	vm.deleteOption = deleteOption;
+	vm.deleteProfile = deleteProfile;
 
+	vm.changeTab = changeTab;
+	vm.changeOptionsType = changeOptionsType;
+	vm.addServer2From = addServer2From;
+	vm.addOption2From = addOption2From;
+	vm.runOption = runOption;
+	vm.pushPlaybook = pushPlaybook;
+	vm.runProfile = runProfile;
 
-	function updateOption( id )
-	{
-		if( id === undefined || id == null  )
-		{
-			keshigSrv.addOption( vm.optionType, getObject(vm.optionType) )
+	keshigSrv.getServerTypes().success(function (data) {
+		vm.serverTypes = data;
+	});
+
+	keshigSrv.getOptionTypes().success(function (data) {
+		vm.optionsType = data;
+	});
+
+	keshigSrv.getBuilds().success(function (data) {
+		for(var i = 0; i < data.length; i++) {
+			var buildDate = new Date(data[i].date);
+			data[i].dateFormated = buildDate.getMonth() + '/' + buildDate.getDate();
 		}
-		else
-		{
-			keshigSrv.updateOption()( vm.optionType, getObject(vm.optionType) )
+		vm.optionsDeployBuilds = data;
+	});
+
+	keshigSrv.getPlaybooks().success(function (data) {
+		vm.playbooks = data;
+	});
+
+	function getProfileValues() {
+		vm.serversByType = [];
+		keshigSrv.getServers().success(function (data) {
+			for(var i = 0; i < data.length; i++) {
+				if(vm.serversByType[data[i].type] === undefined) vm.serversByType[data[i].type] = [];
+				vm.serversByType[data[i].type].push(data[i]);
+			}
+		});
+
+		keshigSrv.getAllOptions().success(function (data) {
+			vm.optionsByType = data;
+		});
+	}
+
+	peerRegistrationService.getResourceHosts().success(function (data) {
+		vm.resourceHosts = [];
+		for(var i = 0; i < data.length; i++) {
+			if(data[i].hostname != 'management') {
+				vm.resourceHosts.push(data[i]);
+			}
+		}
+	});
+
+	function pushPlaybook(playbook) {
+		if(vm.option2Add.playbooks === undefined) vm.option2Add.playbooks = [];
+		if(vm.option2Add.playbooks.indexOf(playbook) >= 0) {
+			vm.option2Add.playbooks.splice(vm.nodes2Action.indexOf(playbook), 1);
+		} else {
+			vm.option2Add.playbooks.push(playbook);
 		}
 	}
 
-	function deleteOption( id )
-	{
-		keshigSrv.deleteOption( id );
-	}
-
-	function deleteServer( id )
-	{
-		keshigSrv.deleteServer( id );
-	}
-
-	function updateServer( id )
-	{
-		var server = {};
-
-		server.serverId = vm.serverId;
-		server.serverName = vm.serverName;
-		server.serverType = vm.serverType;
-
-		if( id === undefined || id == null || !id )
-		{
-			keshigSrv.addServer( server );
-		}
-		else
-		{
-			keshigSrv.updateServer( server );
+	function changeTab(tab) {
+		vm.activeTab = tab;
+		if(vm.activeTab == 'servers') {
+			serversTable();
+		} else if(vm.activeTab == 'options') {
+			changeOptionsType();
+		} else if(vm.activeTab == 'profiles') {
+			getProfileValues();
+			profilesTable();
 		}
 	}
 
-	function getObject( type )
-	{
-		switch( type.toLowerCase() )
-		{
-			case "clone" :
-				break;
-			case "deploy" :
-				break;
-			case "build" :
-				break;
-			case "test" :
-				break;
+	function changeOptionsType() {
+		vm.option2Add = {};
+
+		vm.dtInstance = {};
+		vm.dtOptions = DTOptionsBuilder
+			.fromFnPromise(function() {
+				return $resource(SERVER_URL + 'v1/keshig/option/type/' + vm.optionType).query().$promise;
+			})
+			.withPaginationType('full_numbers')
+			.withOption('stateSave', true)
+			.withOption('order', [[ 1, "asc" ]])
+			.withOption('createdRow', createdRow);
+
+		vm.dtColumns = getOptionTableCol();
+	}
+
+	function serversTable() {
+		vm.dtInstance = {};
+		vm.dtOptions = DTOptionsBuilder
+			.fromFnPromise(function() {
+				return $resource(SERVER_URL + 'v1/keshig/server').query().$promise;
+			})
+			.withPaginationType('full_numbers')
+			.withOption('stateSave', true)
+			.withOption('order', [[ 1, "asc" ]])
+			.withOption('createdRow', createdRow);
+
+		vm.dtColumns = [
+			DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditServer),
+			DTColumnBuilder.newColumn('serverName').withTitle('Name'),
+			DTColumnBuilder.newColumn('type').withTitle('Type'),
+			DTColumnBuilder.newColumn('serverAddress').withTitle('Address'),
+			DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+		];
+	}
+	serversTable();
+
+	function profilesTable() {
+		vm.dtInstance = {};
+		vm.dtOptions = DTOptionsBuilder
+			.fromFnPromise(function() {
+				return $resource(SERVER_URL + 'v1/keshig/profiles').query().$promise;
+			})
+			.withPaginationType('full_numbers')
+			.withOption('stateSave', true)
+			.withOption('order', [[ 0, "asc" ]])
+			.withOption('createdRow', createdRow);
+
+		vm.dtColumns = [
+			//DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditProfile),
+			DTColumnBuilder.newColumn('name').withTitle('Name'),
+			DTColumnBuilder.newColumn('cloneOption').withTitle('Clone').notSortable().renderWith(profileCloneButton),
+			DTColumnBuilder.newColumn('buildOption').withTitle('Build').notSortable().renderWith(profileBuildButton),
+			DTColumnBuilder.newColumn('testOption').withTitle('Test').notSortable().renderWith(profileTestButton),
+			DTColumnBuilder.newColumn('deployOption').withTitle('Deploy').notSortable().renderWith(profileDeployButton),
+			DTColumnBuilder.newColumn('name').withTitle('').notSortable().renderWith(runProfileButton),
+			DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+		];
+	}
+
+	function createdRow(row, data, dataIndex) {
+		$compile(angular.element(row).contents())($scope);
+	}
+
+	function actionEditServer(data, type, full, meta) {
+		vm.servers[data.serverName] = data;
+		return '<a href class="b-icon b-icon_edit" ng-click="keshigCtrl.addServer2From(keshigCtrl.servers[\'' + data.serverName + '\'])"></a>';
+	}
+
+	function actionEditOption(data, type, full, meta) {
+		vm.options[data.name] = data;
+		return '<a href class="b-icon b-icon_edit" ng-click="keshigCtrl.addOption2From(keshigCtrl.options[\'' + data.name + '\'])"></a>';
+	}
+
+	function actionEditProfile(data, type, full, meta) {
+		vm.profiles[data.name] = data;
+		return '<a href class="b-icon b-icon_edit" ng-click="keshigCtrl.addProfile2From(keshigCtrl.options[\'' + data.name + '\'])"></a>';
+	}
+
+	function playbooksTags(data, type, full, meta) {
+		var playbooksHTML = '';
+		for(var i = 0; i < data.length; i++) {
+			playbooksHTML += '<span class="b-tags">' + data[i] + '</span>';
+		}
+		return playbooksHTML;
+	}
+
+	function optionStatusIcon(data, type, full, meta) {
+		return '<div style="width: 100%; text-align: center;"><div class="b-status-icon b-status-icon_' + data + '" tooltips tooltip-title="' + data + '"></div></div>';
+	}
+
+	function runOptionButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_green" ng-click="keshigCtrl.runOption(\'' + data.name + '\')">Run</a>';
+	}
+
+	function profileBuildButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_blue" ng-click="keshigCtrl.runOption(\'' + data + '\', \'build\')">Build</a>';
+	}
+
+	function profileTestButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_blue" ng-click="keshigCtrl.runOption(\'' + data + '\', \'test\')">Test</a>';
+	}
+
+	function profileDeployButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_blue" ng-click="keshigCtrl.runOption(\'' + data + '\', \'deploy\')">Deploy</a>';
+	}
+
+	function profileCloneButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_blue" ng-click="keshigCtrl.runOption(\'' + data + '\', \'clone\')">Clone</a>';
+	}
+
+	function runProfileButton(data, type, full, meta) {
+		return '<a href class="b-btn b-btn_green" ng-click="keshigCtrl.runProfile(\'' + data + '\')">Run</a>';
+	}
+
+	function deleteAction(data, type, full, meta) {
+
+		var action = 'deleteServer';
+		var deleteId = data.serverName;
+		if(vm.activeTab == 'options') {
+			action = 'deleteOption';
+			deleteId = data.name;
+		} else if(vm.activeTab == 'profiles') {
+			action = 'deleteProfile';
+			deleteId = data.name;
+		}
+
+		return '<a href class="b-icon b-icon_remove" ng-click="keshigCtrl.' + action + '(\'' + deleteId + '\')"></a>';
+	}
+
+	function runProfile(profileName) {
+		keshigSrv.startProfile( profileName ).success(function(data){
+			SweetAlert.swal("Success!", '"' + profileName + '" profile start running.', "success");
+		}).error(function (data) {
+			SweetAlert.swal("ERROR!", '"' + profileName + '" profile run error. Error: ' + data.ERROR, 'error');
+		});
+	}
+
+	function addServer2From(server) {
+		vm.server2Add = server;
+		vm.server2Add.serverType = server.type;
+		vm.serverFormUpdate = true;
+	}
+
+	function addOption2From(option) {
+		vm.option2Add = option;
+		vm.optionFormUpdate = true;
+	}
+
+	function addProfile2From(profile) {
+		vm.profiles2Add = profile;
+		vm.profilesFormUpdate = true;
+	}
+
+	function runOption(optionName, customOptionType) {
+		if(customOptionType === undefined || customOptionType === null) customOptionType = vm.optionType;
+		keshigSrv.startOption( customOptionType, optionName ).success(function(data){
+			SweetAlert.swal("Success!", '"' + optionName + '" option start running.', "success");
+			//vm.dtInstance.reloadData(null, false);
+		}).error(function (data) {
+			SweetAlert.swal("ERROR!", '"' + optionName + '" option run error. Error: ' + data.ERROR, 'error');
+		});
+	}
+
+	function updateOption() {
+
+		if(vm.option2Add.targetIps !== undefined) {
+			var targetIps = vm.option2Add.targetIps.split(',');
+			vm.option2Add.targetIps = targetIps;
+		}
+
+		if( vm.optionFormUpdate ) {
+			vm.optionFormUpdate = false;
+			keshigSrv.updateOption( vm.optionType, JSON.stringify(vm.option2Add) ).success(function(data){
+				SweetAlert.swal("Success!", vm.optionType + " option successfully updated.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", vm.optionType + " option update error. Error: " + data.ERROR, "error");
+			});
+		} else {
+			keshigSrv.addOption( vm.optionType, JSON.stringify(vm.option2Add) ).success(function(data){
+				SweetAlert.swal("Success!", vm.optionType + " option successfully added.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", vm.optionType + " option add error. Error: " + data.ERROR, "error");
+			});
+		}
+		vm.option2Add = {};
+	}
+
+	function deleteProfile(profileName) {
+		if(profileName === undefined) return;
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Delete profile " + profileName + "!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Delete",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				keshigSrv.removeProfile(profileName).success(function (data) {
+					SweetAlert.swal("Deleted!", "Your profile has been deleted.", "success");
+					vm.dtInstance.reloadData(null, false);
+				}).error(function (data) {
+					SweetAlert.swal("ERROR!", "Your profile is safe. Error: " + data.ERROR, "error");
+				});
+			}
+		});
+	}
+
+	function deleteOption( optionName )	{
+		if(optionName === undefined) return;
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Delete option " + optionName + "!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Delete",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				keshigSrv.deleteOption(vm.optionType, optionName).success(function (data) {
+					SweetAlert.swal("Deleted!", "Your option has been deleted.", "success");
+					vm.dtInstance.reloadData(null, false);
+				}).error(function (data) {
+					SweetAlert.swal("ERROR!", "Your option is safe. Error: " + data.ERROR, "error");
+				});
+			}
+		});
+	}
+
+	function deleteServer(serverName)	{
+		if(serverName === undefined) return;
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Delete server " + serverName + "!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Delete",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				keshigSrv.removeServer(serverName).success(function (data) {
+					SweetAlert.swal("Deleted!", "Your server has been deleted.", "success");
+					vm.dtInstance.reloadData(null, false);
+				}).error(function (data) {
+					SweetAlert.swal("ERROR!", "Your server is safe. Error: " + data.ERROR, "error");
+				});
+			}
+		});
+	}
+
+	function updateServer( id )	{
+		if(vm.server2Add.serverName === undefined) return;
+		if(vm.server2Add.serverId === undefined) return;
+		if(vm.server2Add.serverType === undefined) return;
+
+		if( vm.serverFormUpdate ) {
+			vm.serverFormUpdate = false;
+			keshigSrv.updateServer( vm.server2Add ).success(function(data){
+				SweetAlert.swal("Success!", "Keshig server successfully updated.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", "Keshig server update error. Error: " + data.ERROR, "error");
+			});
+		} else {
+			keshigSrv.addServer( vm.server2Add ).success(function(data){
+				SweetAlert.swal("Success!", "Keshig server successfully added.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", "Keshig server add error. Error: " + data.ERROR, "error");
+			});
+		}
+		vm.server2Add = {};
+	}
+
+	function updateProfile() {
+		console.log(vm.profiles2Add);
+		if( vm.profilesFormUpdate ) {
+			vm.profilesFormUpdate = false;
+			keshigSrv.updateProfile( JSON.stringify(vm.profiles2Add) ).success(function(data){
+				SweetAlert.swal("Success!", vm.profiles2Add.name + " profile successfully updated.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", vm.profiles2Add.name + " profile update error. Error: " + data.ERROR, "error");
+			});
+		} else {
+			keshigSrv.addProfile( JSON.stringify(vm.profiles2Add) ).success(function(data){
+				SweetAlert.swal("Success!", vm.profiles2Add.name + " profile successfully added.", "success");
+				vm.dtInstance.reloadData(null, false);
+			}).error(function (data) {
+				SweetAlert.swal("ERROR!", vm.profiles2Add.name + " profile add error. Error: " + data.ERROR, "error");
+			});
+		}
+		//vm.profiles2Add = {};
+	}
+
+	function getOptionTableCol() {
+		switch(vm.optionType) {
+			case 'CLONE':
+				return [
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditOption),
+					DTColumnBuilder.newColumn('name').withTitle('Name'),
+					DTColumnBuilder.newColumn('url').withTitle('URL'),
+					DTColumnBuilder.newColumn('branch').withTitle('Branch'),
+					DTColumnBuilder.newColumn('timeOut').withTitle('TimeOut'),
+					DTColumnBuilder.newColumn('active').withTitle('Active').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(runOptionButton),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+				];
+			case 'DEPLOY':
+				return [
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditOption),
+					DTColumnBuilder.newColumn('name').withTitle('Name'),
+					DTColumnBuilder.newColumn('timeOut').withTitle('TimeOut'),
+					DTColumnBuilder.newColumn('active').withTitle('Active').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(runOptionButton),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+				];
+			case 'TEST':
+				return [
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditOption),
+					DTColumnBuilder.newColumn('name').withTitle('Name'),
+					DTColumnBuilder.newColumn('timeOut').withTitle('TimeOut'),
+					DTColumnBuilder.newColumn('playbooks').withTitle('Playbooks').renderWith(playbooksTags),
+					DTColumnBuilder.newColumn('active').withTitle('Active').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(runOptionButton),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+				];
+			case 'BUILD':
+				return [
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionEditOption),
+					DTColumnBuilder.newColumn('name').withTitle('Name'),
+					DTColumnBuilder.newColumn('timeOut').withTitle('TimeOut'),
+					DTColumnBuilder.newColumn('runTests').withTitle('Run Tests').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn('cleanInstall').withTitle('Clean Install').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn('active').withTitle('Active').renderWith(optionStatusIcon),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(runOptionButton),
+					DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(deleteAction)
+				];
 		}
 	}
 }
+
+function checkboxListDropdown() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attr) {
+			$(".b-form-input_dropdown").click(function () {
+				$(this).toggleClass("is-active");
+			});
+
+			$(".b-form-input-dropdown-list").click(function(e) {
+				e.stopPropagation();
+			});
+		}
+	}
+};
+
--- keshig-webui/src/main/webapp/service.js
+++ keshig-webui/src/main/webapp/service.js
@@ -1,137 +1,166 @@
 'use strict';
 
 angular.module('subutai.plugins.keshig.service', [])
-    .factory('keshigSrv', keshigSrv);
+	.factory('keshigSrv', keshigSrv);
 
 keshigSrv.$inject = ['$http'];
 
 function keshigSrv($http) {
 
-    var baseURL = serverUrl + 'keshig/v1';
-	var serverUrl = baseUrl + 'server/';
-    var optionUrl = baseURL + 'option/';
-    var profilesUrl = baseURL + 'profiles/';
+	var baseURL = SERVER_URL + 'v1/keshig/';
+	var serversUrl = baseURL + 'server/';
+	var optionUrl = baseURL + 'option/';
+	var profilesUrl = baseURL + 'profiles/';
 
 
-    var keshigSrv = {
-        getProfiles : getProfiles,
+	var keshigSrv = {
+		getProfiles : getProfiles,
 		addProfile : addProfile,
+		updateProfile : updateProfile,
 		removeProfile : removeProfile,
+		startProfile : startProfile,
 		getServers : getServers,
 		addServer : addServer,
-        removeServer : removeServer,
-        updateServer : updateServer,
-        getServerTypes : getServerTypes,
-        getAllOptions : getAllOptions,
-        getOptionTypes : getOptionTypes,
-        getOptionsByType : getOptionsByType,
-        startOption : startOption,
-        addOption : addOption,
-        updateOption : updateOption
-    };
+		removeServer : removeServer,
+		updateServer : updateServer,
+		getServerTypes : getServerTypes,
+		getAllOptions : getAllOptions,
+		getOptionTypes : getOptionTypes,
+		getOptionsByType : getOptionsByType,
+		startOption : startOption,
+		addOption : addOption,
+		deleteOption : deleteOption,
+		getBuilds: getBuilds,
+		getPlaybooks: getPlaybooks,
+		updateOption : updateOption
+	};
 
-    return keshigSrv;
+	return keshigSrv;
 
-    /*
-    *   Keshig Server Services
-    * */
+	/*
+	 *   Keshig Server Services
+	 * */
 
-	function getProfiles()
-	{
+	function getBuilds() {
+		return $http.get(baseURL + 'build', {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+	}
+
+	function getPlaybooks() {
+		return $http.get(baseURL + 'tests', {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+	}
+
+	function getProfiles() {
 		return $http.get(profilesUrl, {
 			withCredentials: true
 		});
 	}
 
-	function addProfile( profile )
-	{
+	function startProfile(profileName) {
+		return $http.get(profilesUrl + profileName + '/start', {
+			withCredentials: true
+		});
+	}
+
+	function addProfile( profile ) {
 		return $http.post(profilesUrl, profile, {
 			withCredentials: true,
 			headers: {'Content-Type': 'application/json'}
 		});
 	}
 
-	function removeProfile( name )
-	{
-		return $http.delete(profilesUrl + name, {
-			withCredentials: true
-		});
-	}
-
-	function getServers()
-	{
-		return $http.get(serverUrl, {
-			withCredentials: true
-		});
-	}
-
-	function addServer( server )
-	{
-		return $http.post(serverUrl, server, {
+	function updateProfile( profile ) {
+		return $http.put(profilesUrl, profile, {
 			withCredentials: true,
 			headers: {'Content-Type': 'application/json'}
 		});
 	}
 
-	function removeServer(id)
-	{
-		return $http.remove(serverUrl + id, {
+	function removeProfile( name ) {
+		return $http.delete(profilesUrl + name, {
+			withCredentials: true
+		});
+	}
+
+	function getServers() {
+		return $http.get(serversUrl, {
+			withCredentials: true
+		});
+	}
+
+	function addServer( server ) {
+		var postData = 'serverName=' + server.serverName 
+			+ '&serverType=' + server.serverType 
+			+ '&serverId=' + server.serverId;
+		return $http.post(serversUrl, postData, {
+			withCredentials: true,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+	}
+
+	function removeServer(hostId) {
+		return $http.delete(serversUrl + hostId, {
 			withCredentials: true
 		});
 	}
 
 
-    function getServerTypes() {
-        return $http.get(serverUrl + 'types', {
-            withCredentials: true
-        });
-    }
+	function getServerTypes() {
+		return $http.get(serversUrl + 'types', {
+			withCredentials: true
+		});
+	}
 
+	function updateServer( server ) {
+		var postData = 'serverName=' + server.serverName 
+			+ '&serverType=' + server.serverType 
+			+ '&serverId=' + server.serverId;		
+		return $http.put(serversUrl, postData, {
+			withCredentials: true,
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+		});
+	}
 
-    function updateServer( server ) {
-        return $http.put(serverUrl, server, {
-                withCredentials: true,
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-            });
-    }
+	function getAllOptions() {
+		return $http.get(optionUrl, {
+			withCredentials: true
+		});
+	}
 
-    function getAllOptions() {
-        return $http.get(optionUrl, {
-            withCredentials: true
-        });
-    }
+	function getOptionTypes() {
+		return $http.get(optionUrl + 'types', {
+			withCredentials: true
+		});
+	}
 
-    function getOptionTypes() {
-        return $http.get(optionUrl + 'types', {
-            withCredentials: true
-        });
-    }
+	function getOptionsByType(type) {
+		return $http.get(optionUrl + 'type/' + type.toLowerCase(), {
+			withCredentials: true
+		});
+	}
 
-    function getOptionsByType(type) {
-        return $http.get(optionUrl + 'type/' + type, {
-            withCredentials: true
-        });
-    }
+	function deleteOption(type, optionName) {
+		return $http.delete(optionUrl + type.toLowerCase() + '/' + optionName, {
+			withCredentials: true
+		});
+	}
 
-    function startOption(type, optionName) {
-        return $http.get(optionUrl + type + '/' + optionName + '/' + 'start', {
-            withCredentials: true
-        });
-    }
+	function startOption(type, optionName) {
+		return $http.get(optionUrl + type.toLowerCase() + '/' + optionName + '/' + 'start', {
+			withCredentials: true
+		});
+	}
 
-    function addOption( type, object )
-    {
-        return $http.post(optionUrl + type.toLowerCase(), object, {
-            withCredentials: true,
-            headers: {'Content-Type': 'application/json'}
-        });
-    }
+	function addOption( type, object ) {
+		return $http.post(optionUrl + type.toLowerCase(), object, {
+			withCredentials: true,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
 
-    function updateOption( type, object )
-    {
-        return $http.put(optionUrl + type.toLowerCase(), object, {
-            withCredentials: true,
-            headers: {'Content-Type': 'application/json'}
-        });
-    }
+	function updateOption( type, object ) {
+		return $http.put(optionUrl + type.toLowerCase(), object, {
+			withCredentials: true,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
 }
