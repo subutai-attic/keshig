8e8d5e30591c457f79436a13da2f9ed14b314353
kylycht <ktynybekov@gmail.com>
2016-02-22 14:45:36 +0200

    add commit id to version

--- webhook/create_mng.sh
+++ webhook/create_mng.sh
@@ -2,6 +2,7 @@
 
 
 function clone_export {
+
 subutai clone master management
 
 pushd /home/ubuntu > /dev/null
@@ -13,6 +14,7 @@ lxc-attach -n management -- tar -xzf /home/subutai/management.tar.gz
 lxc-attach -n management -- rm -f /var/lib/dhcp/*
 lxc-attach -n management -- apt update
 lxc-attach -n management -- apt install --force-yes -y curl
+
 rm -f /mnt/lib/lxc/lxc-data/management-home/subutai/subutai-4.0.0-RC6.tar.gz
 rm -f /mnt/lib/lxc/lxc-data/management-home/subutai/management.tar.gz
 
@@ -22,14 +24,21 @@ subutai export management
 
 }
 
+function set_version {
+first="subutai.template.version = 4.0.0"
+second="4.0.0-$SHA"
+
+}
 function upload_template {
 
         printf "Preparing to upload template to local Kurjun\n"
         local token=`/apps/subutai/current/bin/curl -s --data "username=admin&password=secret" --insecure https://10.10.10.1:8443/rest/v1/identity/gettoken`
-        /apps/subutai/current/bin/curl -k -F 'package=@/mnt/lib/lxc/lxc-data/tmpdir/management-subutai-template_4.0.0_amd64.tar' -X POST https:10.10.10.1:8443/rest/kurjun/templates/upload/public?sptoken=$token
+        /apps/subutai/current/bin/curl -k -F 'package=@/mnt/lib/lxc/lxc-data/tmpdir/management-subutai-template_4.0.0_amd64.tar.gz' -X POST https:10.10.10.1:8443/rest/kurjun/templates/upload/public?sptoken=$token
         printf "Done transfering "
 
 }
+
+SHA=$1
 
 clone_export
 
